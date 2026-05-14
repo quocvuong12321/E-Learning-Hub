a81be707239eedfc7e8da9872358ea1d5ba005da
King <nguyenquocvuong12321@gmail.com>
2026-05-14 15:58:19 +0700

    feature: implement course crud p4 01

--- e-learning-hub/src/main/java/com/king/lms/e_learning_hub/mapper/CategoryMapper.java
+++ e-learning-hub/src/main/java/com/king/lms/e_learning_hub/mapper/CategoryMapper.java
@@ -4,6 +4,8 @@ package com.king.lms.e_learning_hub.mapper;
 import com.king.lms.e_learning_hub.dto.category.CategoryRequest;
 import com.king.lms.e_learning_hub.dto.category.CategoryResponse;
 import com.king.lms.e_learning_hub.entity.Category;
+import com.king.lms.e_learning_hub.entity.CoursesCategory;
+
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "Spring")
@@ -12,5 +14,10 @@ public interface CategoryMapper {
     Category toCategory(CategoryRequest request);
 
     CategoryResponse toResponse(Category category);
+
+
+    CoursesCategory toCoursesCategory(CategoryRequest request);
+
+    CategoryResponse toResponse(CoursesCategory category);
 
 }
--- e-learning-hub/src/main/java/com/king/lms/e_learning_hub/mapper/mapper.java
+++
@@ -1,4 +0,0 @@
-package com.king.lms.e_learning_hub.mapper;
-
-public class mapper {
-}
